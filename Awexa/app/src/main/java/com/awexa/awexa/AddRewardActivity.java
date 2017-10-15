7f49adcb6faf393a27f546d785cf38369da057d8
delkato <delgado@gatech.edu>
2017-10-15 05:44:47 -0400

    Remove old toolbar crap

--- Awexa/app/src/main/java/com/awexa/awexa/AddRewardActivity.java
+++ Awexa/app/src/main/java/com/awexa/awexa/AddRewardActivity.java
@@ -13,8 +13,6 @@ public class AddRewardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_reward);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
