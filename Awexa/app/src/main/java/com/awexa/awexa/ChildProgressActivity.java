300e1fd6fb6af96b606060ecf0dfb585a2ea1942
delkato <delgado@gatech.edu>
2017-10-15 08:40:18 -0400

    Update title on child progress page with their name

--- Awexa/app/src/main/java/com/awexa/awexa/ChildProgressActivity.java
+++ Awexa/app/src/main/java/com/awexa/awexa/ChildProgressActivity.java
@@ -2,10 +2,7 @@ package com.awexa.awexa;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 public class ChildProgressActivity extends AppCompatActivity {
@@ -13,6 +10,9 @@ public class ChildProgressActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        String child = getIntent().getExtras().getString("name","defaultKey");
+        String title = getString(R.string.child_progress_title, child);
+        setTitle(title);
         setContentView(R.layout.activity_child_progress);
     }
 
