850865718830aa05478a0e0d3018a29e5c7a3a12
Quang Vo <quangdvo@gmail.com>
2017-10-15 16:41:28 -0400

    Added Field/Variable Instantiation to Add Chore Activity
    Added relevant variables to add chore activity in preparation for saving
    chore to database.

--- Awexa/app/src/main/java/com/awexa/awexa/AddChoreActivity.java
+++ Awexa/app/src/main/java/com/awexa/awexa/AddChoreActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -18,10 +19,34 @@ public class AddChoreActivity extends AppCompatActivity {
     }
 
     public void addChore(View view) {
-        EditText text = (EditText)findViewById(R.id.newChoreName);
-        String chore = text.getText().toString();
+        // Instantiate fields and respective variables
+        EditText choreNameField = (EditText)findViewById(R.id.newChoreName);
+        String choreName = choreNameField.getText().toString();
+        EditText rewardPointsField = (EditText)findViewById(R.id.reward);
+        int rewardPoints = Integer.parseInt(rewardPointsField.getText().toString());
+        
+        CheckBox weeklyRepeatCheckbox = (CheckBox)findViewById(R.id.repeatCheck);
+        boolean weeklyRepeat = weeklyRepeatCheckbox.isChecked();
+        CheckBox sunCheckbox = (CheckBox)findViewById(R.id.sunCheck);
+        boolean sunRepeat = sunCheckbox.isChecked();
+        CheckBox monCheckbox = (CheckBox)findViewById(R.id.monCheck);
+        boolean monRepeat = monCheckbox.isChecked();
+        CheckBox tuesCheckbox = (CheckBox)findViewById(R.id.tueCheck);
+        boolean tuesRepeat = tuesCheckbox.isChecked();
+        CheckBox wedCheckbox = (CheckBox)findViewById(R.id.wedCheck);
+        boolean wedRepeat = wedCheckbox.isChecked();
+        CheckBox thursCheckbox = (CheckBox)findViewById(R.id.thuCheck);
+        boolean thursRepeat = thursCheckbox.isChecked();
+        CheckBox friCheckbox = (CheckBox)findViewById(R.id.friCheck);
+        boolean friRepeat = friCheckbox.isChecked();
+        CheckBox satCheckbox = (CheckBox)findViewById(R.id.satCheck);
+        boolean satRepeat = satCheckbox.isChecked();
+
+        EditText descriptionField = (EditText)findViewById(R.id.description);
+        String description = descriptionField.getText().toString();
+
         //TODO: implementation for adding chore to db/list/whatever
-        Toast.makeText(getApplicationContext(), chore + " was added...",
+        Toast.makeText(getApplicationContext(), choreName + " was added...",
                 Toast.LENGTH_SHORT).show();
     }
 }
