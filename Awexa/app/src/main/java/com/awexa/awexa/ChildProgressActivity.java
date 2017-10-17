32241ea539cf2a85e19156fb5185497a0e42ca43
Quang Vo <quangdvo@gmail.com>
2017-10-17 03:41:16 -0400

    Side-Tracked Adv Reward Details Activity
    Committing a potentially side-tracking feature, in which rewards show
    additional information on click on the child's progress activity.

--- Awexa/app/src/main/java/com/awexa/awexa/ChildProgressActivity.java
+++ Awexa/app/src/main/java/com/awexa/awexa/ChildProgressActivity.java
@@ -4,8 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -26,7 +29,22 @@ public class ChildProgressActivity extends AppCompatActivity {
             if (getIntent().getExtras().getString("rewardName") != null) {
                 ArrayAdapter rewardsAdapter = new ArrayAdapter<String>(this,
                         R.layout.activity_listview, rewards);
-                ListView rewardsListView = (ListView)findViewById(R.id.rewards_list);
+                final ListView rewardsListView = (ListView)findViewById(R.id.rewards_list);
+                rewardsListView.setAdapter(rewardsAdapter);
+                
+                /* Todo: implement rewards detail activity on click
+                rewardsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    public void onItemClick(AdapterView<?> parent, View view, int position,
+                                            long id) {
+
+                        Intent intent = new Intent(getApplicationContext(),
+                                ChildProgressActivity.class);
+                        intent.putExtra("name", childName);
+                        startActivity(intent);
+                    }
+                });
+                */
+
             }
         }
         setContentView(R.layout.activity_child_progress);
