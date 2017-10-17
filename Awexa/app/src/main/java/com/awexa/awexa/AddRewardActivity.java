ca3e52309ccac36c1dca928d8bac9930501c8004
Quang Vo <quangdvo@gmail.com>
2017-10-17 01:40:27 -0400

    Add Reward Functionality
    Further adding functionality for adding rewards.

--- Awexa/app/src/main/java/com/awexa/awexa/AddRewardActivity.java
+++ Awexa/app/src/main/java/com/awexa/awexa/AddRewardActivity.java
@@ -1,5 +1,6 @@
 package com.awexa.awexa;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -7,6 +8,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class AddRewardActivity extends AppCompatActivity {
+    String childName = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,10 +17,25 @@ public class AddRewardActivity extends AppCompatActivity {
     }
 
     public void addReward(View view) {
-        EditText text = (EditText)findViewById(R.id.newRewardName);
-        String reward = text.getText().toString();
+        //pull variables from layout
+        EditText nameField = (EditText)findViewById(R.id.newRewardName);
+        String rewardName = nameField.getText().toString();
+        EditText costField = (EditText)findViewById(R.id.cost);
+        int rewardCost = Integer.parseInt(costField.getText().toString());
+        EditText descriptionField = (EditText)findViewById(R.id.description);
+        String rewardDescription = descriptionField.getText().toString();
+
         //TODO: implementation for adding reward to db/list/whatever
-        Toast.makeText(getApplicationContext(), reward + " was added...",
+        Toast.makeText(getApplicationContext(), rewardName + " was added...",
                 Toast.LENGTH_SHORT).show();
+        // pass variables to child progress activity
+        Intent intent = new Intent(getApplicationContext(), ChildProgressActivity.class);
+        childName = getIntent().getExtras().getString("name","defaultKey");
+        intent.putExtra("rewardName", rewardName);
+        intent.putExtra("rewardCost", rewardCost);
+        intent.putExtra("description", rewardDescription);
+        intent.putExtra("name", childName);
+        startActivity(intent);
+
     }
 }
--- Awexa/app/src/main/java/com/awexa/awexa/ChildProgressActivity.java
+++ Awexa/app/src/main/java/com/awexa/awexa/ChildProgressActivity.java
@@ -4,23 +4,40 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
 
 public class ChildProgressActivity extends AppCompatActivity {
+    private String childName = null;
+    private List<String> rewards = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // check for passed extras from intent
         if (getIntent().getExtras() != null) {
-            String child = getIntent().getExtras().getString("name","defaultKey");
-            String title = getString(R.string.child_progress_title, child);
+            // set title as child's name
+            childName = getIntent().getExtras().getString("name","defaultKey");
+            String title = getString(R.string.child_progress_title, childName);
             setTitle(title);
+            // check for new reward
+            if (getIntent().getExtras().getString("rewardName") != null) {
+                ArrayAdapter rewardsAdapter = new ArrayAdapter<String>(this,
+                        R.layout.activity_listview, rewards);
+                ListView rewardsListView = (ListView)findViewById(R.id.rewards_list);
+            }
         }
         setContentView(R.layout.activity_child_progress);
     }
 
     /** Called when the user taps the Add New Reward button */
     public void openNewRewardActivity(View view) {
-        startActivity(new Intent(this, AddRewardActivity.class));
+        // pass child's name back to next activity
+        Intent intent = new Intent(getApplicationContext(), AddRewardActivity.class);
+        intent.putExtra("name", childName);
+        startActivity(intent);
     }
 
     /** Called when the user taps the Add New Chore button */
@@ -28,3 +45,4 @@ public class ChildProgressActivity extends AppCompatActivity {
         startActivity(new Intent(this, AddChoreActivity.class));
     }
 }
+
