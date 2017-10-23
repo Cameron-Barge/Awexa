56c957874f9ddaa72352f0b264d9f1b519fcd5b1
delkato <kdelgado8@gatech.edu>
2017-10-23 10:00:31 -0400

    Add pop up window + logic to load family parents/children

--- Awexa/app/src/main/java/com/awexa/awexa/MainActivity.java
+++ Awexa/app/src/main/java/com/awexa/awexa/MainActivity.java
@@ -2,29 +2,89 @@ package com.awexa.awexa;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.Arrays;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    // Array of random fake kids
-    public List<String> children = Arrays.asList("Garen", "Annie", "Camille");
+    private static final String TAG = "ERROR";
+    DatabaseReference db = FirebaseDatabase.getInstance().getReference();
+    String currentFamily = "family1";
+    DatabaseReference ref = db.child("families/" + currentFamily);
+    public List<String> family = new ArrayList<>();
+    public List<String> parents = new ArrayList<>();
+    public String familyPass = "";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        DatabaseReference parentIDRef = db.child("families/" + currentFamily + "/parents");
+        parentIDRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for(DataSnapshot singleSnapshot : dataSnapshot.getChildren()){
+                    DatabaseReference parentRef = db.child("parents/" + singleSnapshot.getKey() + "/name");
+                    parentRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot parentSnapshot) {
+                            family.add((String) parentSnapshot.getValue());
+                            parents.add((String) parentSnapshot.getValue());
+                            Toast.makeText(getApplicationContext(), (String) parentSnapshot.getValue(),
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+                            Log.e(TAG, "onCancelled", databaseError.toException());
+                        }
+                    });
+                }
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.e(TAG, "onCancelled", databaseError.toException());
+            }
+        });
+
+        DatabaseReference childRef = db.child("families/" + currentFamily + "/child_names");
+        childRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for(DataSnapshot singleSnapshot : dataSnapshot.getChildren()){
+                    Toast.makeText(getApplicationContext(), (String) singleSnapshot.getKey(),
+                            Toast.LENGTH_SHORT).show();
+                    family.add(singleSnapshot.getKey());
+                }
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.e(TAG, "onCancelled", databaseError.toException());
+            }
+        });
+
         ArrayAdapter adapter = new ArrayAdapter<String>(this,
-                R.layout.activity_listview, children);
+                R.layout.activity_listview, family);
 
         final ListView listView = (ListView) findViewById(R.id.children_list);
         listView.setAdapter(adapter);
@@ -32,11 +92,14 @@ public class MainActivity extends AppCompatActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 String name = ((TextView) view).getText().toString();
-                Toast.makeText(getApplicationContext(), name,
-                        Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(MainActivity.this, ChildProgressActivity.class);
-                intent.putExtra("name", name);
-                startActivity(intent);
+                if (parents.contains(name)) {
+                    Toast.makeText(getApplicationContext(), name + " is a parent...",
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    Intent intent = new Intent(MainActivity.this, ChildProgressActivity.class);
+                    intent.putExtra("name", name);
+                    startActivity(intent);
+                }
             }
         });
 
@@ -47,5 +110,32 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, AddChildActivity.class));
             }
         });
+    }
+
+    public void showPopupWindow(View view) {
+        // get a reference to the already created main layout
+        CoordinatorLayout mainLayout = (CoordinatorLayout) findViewById(R.id.activity_main_layout);
+
+        // inflate the layout of the popup window
+        LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+        View popupView = inflater.inflate(R.layout.popup_window, null);
+
+        // create the popup window
+        int width = CoordinatorLayout.LayoutParams.WRAP_CONTENT;
+        int height = CoordinatorLayout.LayoutParams.WRAP_CONTENT;
+        boolean focusable = true; // lets taps outside the popup also dismiss it
+        final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+
+        // show the popup window
+        popupWindow.showAtLocation(mainLayout, Gravity.CENTER, 0, 0);
+
+        // dismiss the popup window when touched
+        /*popupView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                popupWindow.dismiss();
+                return true;
+            }
+        });*/
     }
 }
