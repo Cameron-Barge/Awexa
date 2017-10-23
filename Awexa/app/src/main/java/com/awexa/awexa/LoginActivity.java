401fa13d8a2bf5c4a5bc198953974880841d28a4
delkato <kdelgado8@gatech.edu>
2017-10-23 18:10:39 -0400

    Login/Registration + Sign out works

--- Awexa/app/src/main/java/com/awexa/awexa/LoginActivity.java
+++ Awexa/app/src/main/java/com/awexa/awexa/LoginActivity.java
@@ -10,7 +10,11 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
 import com.google.firebase.auth.FirebaseUser;
 
 /**
@@ -27,7 +31,7 @@ public class LoginActivity extends AppCompatActivity {
     private FirebaseAuth.AuthStateListener mAuthListener;
 
     private EditText mEmail, mPassword;
-    private Button btnSignIn;
+    private Button btnSignIn, btnSignOut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +41,7 @@ public class LoginActivity extends AppCompatActivity {
         mEmail = (EditText) findViewById(R.id.email);
         mPassword = (EditText) findViewById(R.id.password);
         btnSignIn = (Button) findViewById(R.id.email_sign_in_button);
+        btnSignOut = (Button) findViewById(R.id.email_sign_out_button);
 
         mAuth = FirebaseAuth.getInstance();
 
@@ -64,10 +69,38 @@ public class LoginActivity extends AppCompatActivity {
                 String email = mEmail.getText().toString();
                 String pass = mPassword.getText().toString();
                 if(!email.equals("") && !pass.equals("")){
-                    mAuth.signInWithEmailAndPassword(email,pass);
+                    mAuth.signInWithEmailAndPassword(email, pass)
+                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Log.d(TAG, "signInWithEmail:success");
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.w(TAG, "signInWithEmail:failure", task.getException());
+                                    try {
+                                        throw task.getException();
+                                    } catch (FirebaseAuthInvalidUserException e) {
+                                        mAuth.createUserWithEmailAndPassword(mEmail.getText().toString(), mPassword.getText().toString());
+                                        toastMessage("User created successfully...");
+                                    } catch (Exception e) {
+                                        Log.e(TAG, e.getMessage());
+                                    }
+                                }
+                            }
+                        });
                 }else{
                     toastMessage("You didn't fill in all of the fields.");
                 }
+            }
+        });
+
+        btnSignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAuth.signOut();
+                toastMessage("Signing Out...");
             }
         });
     }
