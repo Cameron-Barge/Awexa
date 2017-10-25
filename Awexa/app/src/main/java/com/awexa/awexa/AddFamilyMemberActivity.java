0529e2e173bb375472be937f282ecff2b4ac0b98
delkato <kdelgado8@gatech.edu>
2017-10-25 16:42:50 -0400

    Adding a child works now

--- Awexa/app/src/main/java/com/awexa/awexa/AddFamilyMemberActivity.java
+++ Awexa/app/src/main/java/com/awexa/awexa/AddFamilyMemberActivity.java
@@ -8,32 +8,49 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class AddFamilyMemberActivity extends AppCompatActivity {
     private RadioGroup radioFamilyMemberGroup;
     private RadioButton radioFamilyMemberButton;
     private String familyMemberType;
+    DatabaseReference db = FirebaseDatabase.getInstance().getReference();
+    String currentFamily = "";
+    EditText name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_family_member);
+
+        currentFamily = FirebaseAuth.getInstance().getCurrentUser().getUid();
     }
 
     /** Called when the user taps the Submit button */
     public void addFamilyMember(View view) {
-        EditText text = findViewById(R.id.familyMemberName);
-        String familyMember = text.getText().toString();
+        name = findViewById(R.id.familyMemberName);
+        String familyMemberName = name.getText().toString();
         radioFamilyMemberGroup = findViewById(R.id.radioFamilyMember);
         int selectedId = radioFamilyMemberGroup.getCheckedRadioButtonId();
         radioFamilyMemberButton = findViewById(selectedId);
         familyMemberType = radioFamilyMemberButton.getText().toString();
         if (radioFamilyMemberButton.getText().toString().equals("Child")) {
-            //TODO: implementation for adding child to db
-            Toast.makeText(getApplicationContext(), familyMemberType + " " + familyMember + " was added...",
+            Child child = new Child(familyMemberName, currentFamily);
+            DatabaseReference childRef = db.child("children");
+            childRef.push().setValue(child);
+            DatabaseReference keyRef = childRef.push();
+            String childID = keyRef.getKey();
+            DatabaseReference childNamesRef = db.child("families/" + currentFamily + "/child_names/" + familyMemberName);
+            childNamesRef.setValue(childID);
+            DatabaseReference childrenRef = db.child("families/" + currentFamily + "/children/" + childID);
+            childrenRef.setValue(true);
+            Toast.makeText(getApplicationContext(), familyMemberType + " " + familyMemberName + " was added...",
                     Toast.LENGTH_SHORT).show();
         } else if (radioFamilyMemberButton.getText().toString().equals("Parent")) {
             //TODO: implementation for adding parent to db
-            Toast.makeText(getApplicationContext(), familyMemberType + " " + familyMember + " was added...",
+            Toast.makeText(getApplicationContext(), familyMemberType + " " + familyMemberName + " was added...",
                     Toast.LENGTH_SHORT).show();
         } else {
             Toast.makeText(getApplicationContext(), "Please select a radio button.",
