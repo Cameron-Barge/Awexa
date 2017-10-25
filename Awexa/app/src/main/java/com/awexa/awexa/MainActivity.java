e6e72b32ce96f564fcbf0c56b9169843bf1b6092
delkato <kdelgado8@gatech.edu>
2017-10-25 15:56:48 -0400

    Change Add Child to Add Family Member

--- Awexa/app/src/main/java/com/awexa/awexa/MainActivity.java
+++ Awexa/app/src/main/java/com/awexa/awexa/MainActivity.java
@@ -123,7 +123,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, AddChildActivity.class));
+                startActivity(new Intent(MainActivity.this, AddFamilyMemberActivity.class));
             }
         });
     }
